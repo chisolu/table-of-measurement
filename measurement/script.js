51812cc37add7c49ef8acf2438c305ea30ffdea2
Ucheonye C . H Umeotawara <uchisolu@gmail.com>
2025-05-12 10:07:19 +0100

    Add files via upload

---
+++ measurement/script.js
@@ -0,0 +1,12 @@
+
+const btn =document.getElementById('btn');
+const cardone =document.querySelectorAll('#card-one');
+//const btn =document.getElementById('btn');
+
+const result = document.getElementById('result');
+
+btn.addEventListener('click', () => {
+   cardone.forEach(function(value){
+    console.log(cardone);
+   });
+});
